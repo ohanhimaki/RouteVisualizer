9f8e20d274f51e703d884cb08b4425f5224db858
Olli Hanhimäki <ohanhimaki@gmail.com>
2020-06-27 08:59:20 +0300

    InitCommit

---
+++ RouteVisualizer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RouteVisualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testifilu = "C:\\coding\\RouteVisualizer\\testifilut\\2020-06-14T1508340300PT1H57M49905SPyoraily.tcx";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
